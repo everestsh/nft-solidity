dbbb267f2681790cb17da4edeaec0dedc0d32fc2
Mingjun Geng <sooofeve@gmail.com>
2022-08-18 12:43:21 -0700

    beforeEach use async/await test pass print

--- the-first/test/Inbox.test.js
+++ the-first/test/Inbox.test.js
@@ -4,6 +4,7 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+let accounts;
 beforeEach(async()=>{
     // Get a list of all accounts
     // web3.eth.getAccounts().then( fetchedAccounts => {
@@ -16,6 +17,6 @@ beforeEach(async()=>{
 
 describe('Inbox', ()=>{
    it('deploys a contract', ()=>{
-
+        console.log(accounts);
     })
 })
