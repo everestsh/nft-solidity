8dfe78eb74b8e722aa70a146cec0b2ea57f9c8fa
Mingjun Geng <sooofeve@gmail.com>
2022-08-26 10:31:05 -0700

    :Lottery: connect matamask init

--- lottery-react/src/web3.js
+++ lottery-react/src/web3.js
@@ -1,18 +1,8 @@
 import Web3 from "web3";
-
-console.log('11111111!');
-
+window.ethereum.request({ method: "eth_requestAccounts" });
+if (typeof window.ethereum !== 'undefined') {
+    console.log('MetaMask is installed!');
+}
 const web3 = new Web3(window.ethereum);
-var account_global
-const accounts = window.ethereum.request({ method: 'eth_requestAccounts' }); // Waits for connection to MetaMask.
-account_global = accounts[0];
-// if (typeof window.ethereum == 'undefined') {
-//     console.log('MetaMask is installed!');
-// }
-console.log(accounts)
-console.log('222222!');
-// ethereum.request({ method: "eth_requestAccounts" });
-
-
 
 export default web3;
